34fad389c6711e5d0cb2f1a766d6ecb3ac0a4f5e
Dillon McGuire <DMMcGuire6724@gmail.com>
2015-11-28 15:40:09 -0500

    api call done

---
+++ public/angular.js
@@ -0,0 +1,208 @@
+var app = angular.module("DrinkApp", ["ngRoute"]);
+
+app.controller('RouteController', ['$http', '$scope', '$route', '$routeParams', '$location',
+function($http, $scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+}]);
+
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    $routeProvider.
+    when('/', {
+      templateUrl: 'templates/bar.html',
+      controller: 'BarController',
+      controllerAs: 'barCtrl'
+    }).
+    when('/clubs', {
+      templateUrl: 'templates/clubs.html',
+      controller: 'ClubController',
+      controllerAs: 'clubCtrl'
+    })
+}]);
+
+
+var latitude;
+var longitude;
+// var googlekey = 'AIzaSyBAuDke1dC1ZniEqPJ_Rv7wfbo3zJbED2w', "AIzaSyAtuHwPlG6AvcHUugdhp99dT1FCAEWSSWs"
+
+var yelps = [];
+var auth = {
+    consumerKey: "-7RT9CltQThTiOwIYunmzA",
+    consumerSecret: "NwMK4NInbjL13e5gY2VUWD7nGBw",
+    accessToken: "ZP1lbNJ-TVxJuvwHD2YzLZHUX4i5LZKn",
+    accessTokenSecret: "MaDSeYpCt4cPeWsbKD6yzqopwtY",
+    serviceProvider: {
+        signatureMethod: "HMAC-SHA1"
+    }
+};
+
+app.controller("BarController", [ "$http", "$scope", function ($http, $scope) {
+  ctrl = this
+
+  ctrl.color = function () {
+    var colorArray =  [ '#C2F0E9', '#D2C2F0',  '#E0F0C2',  '#F0C2C9',  '#F0DFC2',  '#C2C3F0', '#E1C2F0',  '#D1F0C2',  '#C1C1E3',  '#C1E0E3',  '#E3ACAC', '#E3ACD4',  '#ACE3C0',  '#ACE3D5',  '#E3C2AC',  '#E3ACCB', '#D2C2F0',  '#E0F0C2',  '#F0C2C9',  '#F0DFC2']
+    var num = Math.floor((Math.random() * colorArray.length));
+    return colorArray[num]
+  }
+
+  ctrl.icon = function () {
+    var iconArray =  ["cocktail.png", "wine.png"]
+    var num = Math.floor((Math.random() * iconArray.length));
+    return iconArray[num]
+  }
+
+  this.metersToMiles = function (m) {
+    var miles = (m * 0.00062137).toFixed(2);
+    return miles
+  }
+
+  var callBars = function () {
+    var terms = "bars"
+    console.log("CALLING BARS");
+    var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+    };
+    parameters = [];
+    parameters.push(['term', terms]);
+    parameters.push(['ll', latitude + ", " + longitude]);
+    parameters.push(['limit', 20]);
+    parameters.push(['sort', 1]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+    var message = {
+        'action': 'http://api.yelp.com/v2/search',
+        'method': 'GET',
+        'parameters': parameters
+    };
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+
+    // send the request to yelp
+    $.ajax({
+      'url': message.action,
+      'data': parameterMap,
+      'cache': true,
+      'dataType': 'jsonp',
+      'jsonpCallback': 'cb'
+    }).success(function(data, textStats, XMLHttpRequest) {
+        $scope.$apply(function () {
+          $('.loading-container').empty();
+          $scope.barCtrl.bars = data.businesses;
+          console.log(data.businesses);
+
+        });
+    })
+  } // end of callBars function
+
+
+  // Root will automatically post all bars near you
+  $http.get("/").success(function () {
+    function geo_success(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      callBars();
+    }
+
+    function geo_error(error) {
+     alert('ERROR(' + error.code + '): ' + error.message);
+    };
+
+    var geo_options = {
+      enableHighAccuracy: true,
+      maximumAge        : 30000,
+      timeout           : 27000
+    };
+
+    navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+  })
+}])
+
+
+
+
+
+app.controller("ClubController", [ "$http", "$scope", function ($http, $scope) {
+  ctrl = this
+
+  this.metersToMiles = function (m) {
+    var miles = (m * 0.00062137).toFixed(3);
+    return miles
+  }
+
+  var callClubs = function () {
+    var terms = "clubs"
+    console.log("CALLING CLUBS");
+    var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+    };
+    parameters = [];
+    parameters.push(['term', terms]);
+    parameters.push(['ll', latitude + ", " + longitude]);
+    parameters.push(['limit', 20]);
+    parameters.push(['sort', 1]);
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+    var message = {
+        'action': 'http://api.yelp.com/v2/search',
+        'method': 'GET',
+        'parameters': parameters
+    };
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+
+    // send the request to yelp
+    $.ajax({
+      'url': message.action,
+      'data': parameterMap,
+      'cache': true,
+      'dataType': 'jsonp',
+      'jsonpCallback': 'cb'
+    }).success(function(data, textStats, XMLHttpRequest) {
+        $scope.$apply(function () {
+          $scope.clubCtrl.clubs = data.businesses;
+          console.log(data.businesses);
+
+        });
+    })
+  } // end of callBars function
+
+
+  // Root will automatically post all bars near you
+  $http.get("/").success(function () {
+    function geo_success(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      callClubs();
+    }
+
+    function geo_error(error) {
+     alert('ERROR(' + error.code + '): ' + error.message);
+    };
+
+    var geo_options = {
+      enableHighAccuracy: true,
+      maximumAge        : 30000,
+      timeout           : 27000
+    };
+
+    navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+  })
+}])
---
+++ server.js
@@ -0,0 +1,103 @@
+var mongoose = require('mongoose'),
+    server   = require('./lib/create-server')(),
+    bcrypt   = require('bcrypt'),
+    PORT     = process.env.PORT || 3000,
+    dbname   = 'drinkr'
+    MONGOURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
+    Schema   = mongoose.Schema,
+    User     = require('./models/user.js');
+
+var verifyLogIn = function (req, res, next) {
+	if (req.session.currentUser){
+		next();
+	} else {
+		res.redirect(302, "/login");
+	}
+};
+
+
+///////////////////////////////////////////////////////////
+/////////////           ROUTES             ////////////////
+///////////////////////////////////////////////////////////
+
+// when a user logs in
+server.post('/session', function (req, res) {
+  req.body.user.email = req.body.user.email.toLowerCase();
+  User.findOne({email: req.body.user.email}, function (err, currentUser) {
+    if (err) {
+        console.log(err);
+    } else {
+        if (currentUser === null) {
+          req.session.flash.userDoesntExist = "Incorrect Email/Password combo";
+          res.redirect(302, '/login')
+        } else {
+          bcrypt.compare(req.body.user.password, currentUser.password, function (err, match) {
+            if (err) {
+              console.log(err);
+            } else if (!match) {
+              req.session.flash.incorrectPassword = "Incorrect Email/Password combo";
+              res.redirect(302, '/login')
+            } else {
+              req.session.currentUser = req.body.user.email;
+            }  res.redirect(302, '/')
+          })
+        }
+    };
+  });
+});
+
+// when a new user is created
+server.post('/user/new', function (req, res) {
+  if (!req.body.user.password) {
+    req.session.flash.needPassword = "Please enter an password";
+    res.redirect(302, '/login');
+  } else if (!req.body.user.email) {
+    req.session.flash.needUser = "Please enter a valid email";
+    res.redirect(302, '/login');
+  } else{
+    var userInfo = req.body.user
+  	req.session.currentUser = userInfo.email;
+  	var newUser = new User(userInfo)
+
+    //save the user iusing infor from the params
+    newUser.save(function (err, createdUser) {
+      if (err) {
+        if (err.code === 11000) {
+          req.session.flash.duplicateName = "Email is taken";
+          res.redirect(302, '/login');
+        } else {
+          console.log(err);
+        }
+      } else {
+        req.session.currentUser = req.body.user.email
+        res.redirect(302, '/')
+      }
+    });
+  }
+});
+
+
+server.get("/login", function(req, res) {
+  res.sendFile("login.html", {
+    root:"public"
+  })
+})
+
+// get index if verifyLogIn returns true or it redirects to login page
+server.get("/", verifyLogIn, function (req, res) {
+  res.sendFile("index.html", {
+    root: __dirname + "/public/"
+  })
+})
+
+server.get("/clubs", verifyLogIn, function (req, res) {
+  res.sendFile("index.html", {
+    root: __dirname + "/public/"
+  })
+})
+
+
+mongoose.connect(MONGOURI + "/" + dbname);
+  server.listen(PORT, function () {
+    console.log("\n Hello, it's me on ", PORT);
+});
